0a334c88cbac4518669bf40c334e7f8838973c29
cr-arya-2024 <crarya2024@gmail.com>
2025-08-23 19:31:19 +0530

    completed frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-  const[currentState,setCurrentState]=useState('Sign-up')
+  const[currentState,setCurrentState]=useState('Login')
+  const onSubmitHandler=(e)=>{
+    e.preventdefault()
+
+  }
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w96 m-auto mt-14 gap-4 text-gray-800' action="onSubmit">
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800' onSubmit={onSubmitHandler}>
       <div className='inline-flex gap-2 mb-2 mt-10'>
       <p className='mozilla-headline-<uniquifier> text-3xl'>{currentState}</p>
       <hr className='border-none mt-5 h-[2px] w-8 bg-gray-800'/ >
       </div>
-      <input type="text" placeholder='Name' className='border border-gray-300 rounded py-1.5 px-5 w-full  sm:w-1/4' />
-      <input type="email" placeholder=' Email' className='border border-gray-300 rounded py-1.5 px-5 w-full  sm:w-1/4' />
-      <input type="password" placeholder='Password' className='border border-gray-300 rounded py-1.5 px-5 w-full  sm:w-1/4' />
+     {currentState!=='Login'? <input type="text" placeholder='Name' className='border border-gray-800  rounded py-1.5 px-5 w-full' />:''}
+      <input type="email" placeholder=' Email' className='border border-gray-800  rounded py-1.5 px-5 w-full' />
+      <input type="password" placeholder='Password' className='border border-gray-800  rounded py-1.5 px-5 w-full ' />
+  <div className='w-full flex justify-between text-sm mt-[-8px]'>
+<p className='cursor-pointer'>Forgot Password?</p>
+{
+  currentState==='Login'
+  ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create account</p>:<p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+}
+  </div>
+  <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState==='Login'?'Sign In':'Sign Up'}</button>
     </form>
   )
 }
